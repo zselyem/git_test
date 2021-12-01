3a3857cc8d0377bc794d66b19c12292d90c6a856
diak <diak@28-17>
2021-12-01 19:05:02 +0100

    Add project files.

---
+++ git_test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
